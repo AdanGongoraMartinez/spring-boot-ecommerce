483eb6433b4d43c5b99ccd40a14d48c860952314
adan-local <adangonmar@gmail.com>
2025-01-04 22:28:58 -0600

    Adding font familly

--- ecomerce/apps/ecom-frontend/tailwind.config.js
+++ ecomerce/apps/ecom-frontend/tailwind.config.js
@@ -8,6 +8,19 @@ module.exports = {
     ...createGlobPatternsForDependencies(__dirname),
   ],
   theme: {
+    fontFamily: {
+      'sans': 'Inter var, ui-sans-serif, system-ui',
+      'serif': 'Inter var, ui-sans-serif, system-ui',
+    },
+    fontSize: {
+      sm: '0.875rem',
+      base: '1.3rem',
+      xl: '1.55rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
+    },
     extend: {},
   },
   plugins: [],
